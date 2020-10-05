fde7b67113d9367ed9a1ba0a12c0fc43581e50d0
Administrador <magalhaesro1979@gmail.com>
2020-10-05 17:16:51 -0300

    correction in BackSql.php

--- examples/allTables.php
+++ examples/allTables.php
@@ -6,5 +6,4 @@ require_once __DIR__."/../ext_lib/autoload.php";
 
 $bk = new BackupSql(); // new instance
 $fileName = 'backup'.date('d-m-Y'); //define a file name
-$bk->createAllTables($fileName,"zip"); //insert the table numbers to be backed up
-// compress = zip | gz | gzip
+$bk->createAllTables($fileName); //insert the table numbers to be backed up
