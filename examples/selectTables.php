484b3dfacb301fca427a63f397934eec85f0dcb2
Administrador <magalhaesro1979@gmail.com>
2020-10-06 13:06:27 -0300

    several corrections and improvements in methods

--- examples/selectTables.php
+++ examples/selectTables.php
@@ -6,5 +6,5 @@ require_once __DIR__."/../ext_lib/autoload.php";
 
 $fileName = 'backup'.date('Y-m-d'); //define a file name
 $bk = new BackupSql($fileName); // new instance
-$bk->createSelectTables(["tbexpositor_feiras","tbcadgeral_feiras"]); //insert the table numbers to be backed up
+$bk->createSelectTables(["table_name","table_name1"]); //insert the table numbers to be backed up
 // compress = zip | gz | gzip
